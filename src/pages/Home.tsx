dbb7cdb1903cb12d488e711272a96d9e83bfae87
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-12 18:20:22 +0530

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+
+    <div
+      style={{
+        background: "#000",
+        color: "#fff",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "60px",
+        fontWeight: "bold"
+      }}
+    >
+
+      FUTURE HIT V3
+
+    </div>
+
+  );
+}
