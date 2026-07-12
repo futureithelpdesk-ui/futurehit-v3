c9d05b33764858b2c5a0463bb996dc9d294b723e
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-13 01:02:38 +0530

    Create HeroHUD.tsx

---
+++ src/components/hero/HeroHUD.tsx
@@ -0,0 +1,37 @@
+import "./Hero.css";
+
+export default function HeroHUD(){
+
+return(
+
+<>
+
+<div className="hud hud-top-left">
+
+SYSTEM ONLINE
+
+</div>
+
+<div className="hud hud-top-right">
+
+SECURE CHANNEL
+
+</div>
+
+<div className="hud hud-bottom-left">
+
+FUTURE V3
+
+</div>
+
+<div className="hud hud-bottom-right">
+
+STATUS : ACTIVE
+
+</div>
+
+</>
+
+)
+
+}
