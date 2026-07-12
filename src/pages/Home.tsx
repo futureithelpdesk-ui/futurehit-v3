4fe5037e89e5fac7b54dee39689f97db6bb0b266
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-13 00:55:00 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,31 @@
 import { useState } from "react";
+
 import BootLoader from "../components/loading/BootLoader";
+
 import MatrixRain from "../components/effects/MatrixRain";
-import GlitchText from "../components/effects/GlitchText";
 
-export default function Home() {
-  const [loaded, setLoaded] = useState(false);
+import Hero from "../components/hero/Hero";
 
-  if (!loaded) {
-    return <BootLoader onComplete={() => setLoaded(true)} />;
-  }
+export default function Home(){
 
-  return (
-    <>
-      <MatrixRain />
+const [loaded,setLoaded]=useState(false);
 
-      <main
-        style={{
-          position: "relative",
-          zIndex: 2,
-          minHeight: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          background: "#000",
-          color: "#fff",
-        }}
-      >
-        <GlitchText text="FUTURE" />
+if(!loaded){
 
-        <p
-          style={{
-            marginTop: "20px",
-            color: "#aaa",
-            fontSize: "20px",
-          }}
-        >
-          Welcome to FutureHit V3
-        </p>
-      </main>
-    </>
-  );
+return <BootLoader onComplete={()=>setLoaded(true)}/>
+
+}
+
+return(
+
+<>
+
+<MatrixRain/>
+
+<Hero/>
+
+</>
+
+)
+
 }
