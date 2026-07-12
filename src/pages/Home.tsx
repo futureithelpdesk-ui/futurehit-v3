06ff5c96f086b7a1e5cd18cb76bd91df43d061ae
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-13 00:34:52 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import BootLoader from "../components/loading/BootLoader";
+import MatrixRain from "../components/effects/MatrixRain";
 
 export default function Home() {
   const [loaded, setLoaded] = useState(false);
@@ -9,34 +10,41 @@ export default function Home() {
   }
 
   return (
-    <main
-      style={{
-        minHeight: "100vh",
-        background: "#050505",
-        color: "#00ff66",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <h1
-        style={{
-          fontSize: "64px",
-          letterSpacing: "5px",
-        }}
-      >
-        FUTURE
-      </h1>
+    <>
+      <MatrixRain />
 
-      <p
+      <main
         style={{
-          marginTop: "15px",
-          color: "#aaa",
+          position: "relative",
+          zIndex: 2,
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          background: "#000",
+          color: "#00ff66",
         }}
       >
-        Welcome to FutureHit V3
-      </p>
-    </main>
+        <h1
+          style={{
+            fontSize: "70px",
+            letterSpacing: "8px",
+          }}
+        >
+          FUTURE
+        </h1>
+
+        <p
+          style={{
+            color: "#aaaaaa",
+            marginTop: "20px",
+            fontSize: "20px",
+          }}
+        >
+          Welcome to FutureHit V3
+        </p>
+      </main>
+    </>
   );
 }
