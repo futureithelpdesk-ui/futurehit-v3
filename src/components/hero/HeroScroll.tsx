3e31bc2aa8e90335c261cb32fe477744af0c6078
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-13 01:01:59 +0530

    Create HeroScroll.tsx

---
+++ src/components/hero/HeroScroll.tsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+export default function HeroScroll() {
+  return (
+    <div className="scroll-indicator">
+
+      <span></span>
+
+      <p>SCROLL</p>
+
+    </div>
+  );
+}
