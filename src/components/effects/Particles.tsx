574dd99cb5f321161b49525ae7f978dcb65f4e33
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-13 23:28:42 +0530

    Create Particles.tsx

---
+++ src/components/effects/Particles.tsx
@@ -0,0 +1,34 @@
+import { useMemo } from "react";
+import "./Particles.css";
+
+export default function Particles() {
+  const particles = useMemo(
+    () =>
+      Array.from({ length: 60 }, (_, i) => ({
+        id: i,
+        left: Math.random() * 100,
+        delay: Math.random() * 8,
+        duration: 6 + Math.random() * 10,
+        size: 2 + Math.random() * 5,
+      })),
+    []
+  );
+
+  return (
+    <div className="particles">
+      {particles.map((p) => (
+        <span
+          key={p.id}
+          className="particle"
+          style={{
+            left: `${p.left}%`,
+            animationDelay: `${p.delay}s`,
+            animationDuration: `${p.duration}s`,
+            width: `${p.size}px`,
+            height: `${p.size}px`,
+          }}
+        />
+      ))}
+    </div>
+  );
+}
