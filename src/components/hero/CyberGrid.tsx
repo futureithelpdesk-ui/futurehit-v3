0fb303481c4e88db7d0dbb9bd4d86ce04d8a6b07
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-13 23:47:01 +0530

    Create CyberGrid.tsx

---
+++ src/components/hero/CyberGrid.tsx
@@ -0,0 +1,13 @@
+import "./CyberGrid.css";
+
+export default function CyberGrid() {
+  return (
+    <div className="cyber-grid">
+
+      <div className="grid"></div>
+
+      <div className="grid glow"></div>
+
+    </div>
+  );
+}
