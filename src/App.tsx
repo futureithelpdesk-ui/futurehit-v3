9a2f36982d9667db4b4e725740b2c3a8895b6a3c
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-13 23:40:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={<Home />}
+        />
       </Routes>
     </BrowserRouter>
   );
