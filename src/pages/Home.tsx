7bef13d10887c86a5f8e00953e5311bdc57e6618
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-13 23:30:58 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
 import BootLoader from "../components/loading/BootLoader";
+
 import MatrixRain from "../components/effects/MatrixRain";
+import Particles from "../components/effects/Particles";
+
 import Hero from "../components/hero/Hero";
 
 export default function Home() {
@@ -17,12 +20,19 @@ export default function Home() {
   }
 
   return (
-    <main>
-
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#000",
+        overflow: "hidden",
+        position: "relative",
+      }}
+    >
       <MatrixRain />
 
-      <Hero />
+      <Particles />
 
+      <Hero />
     </main>
   );
 }
