3fda7fefe1e70e7ab69bb848b9b0dd20279de88f
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-14 17:02:35 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,11 @@
-import { useState } from "react";
-
-import BootLoader from "../components/loading/BootLoader";
-
-import MatrixRain from "../components/effects/MatrixRain";
-import Particles from "../components/effects/Particles";
-import MouseGlow from "../components/effects/MouseGlow";
-import Scanlines from "../components/effects/Scanlines";
-
 import Hero from "../components/hero/Hero";
+import About from "../components/about/About";
 
 export default function Home() {
-  const [loaded, setLoaded] = useState(false);
-
-  if (!loaded) {
-    return (
-      <BootLoader
-        onComplete={() => setLoaded(true)}
-      />
-    );
-  }
-
   return (
-    <main
-      style={{
-        position: "relative",
-        minHeight: "100vh",
-        background: "#000",
-        overflowX: "hidden",
-      }}
-    >
-      <MatrixRain />
-
-      <Particles />
-
-      <MouseGlow />
-
-      <Scanlines />
-
+    <>
       <Hero />
-    </main>
+      <About />
+    </>
   );
 }
