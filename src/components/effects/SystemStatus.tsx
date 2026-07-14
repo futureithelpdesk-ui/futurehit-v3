9638f6c204a2d6fe4361184edc0211a86e5df757
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-14 19:35:27 +0530

    Create SystemStatus.tsx

---
+++ src/components/effects/SystemStatus.tsx
@@ -0,0 +1,37 @@
+import "./SystemStatus.css";
+
+export default function SystemStatus(){
+
+return(
+
+<div className="system-status">
+
+<div>
+
+<span className="dot"></span>
+
+SYSTEM ONLINE
+
+</div>
+
+<div>
+
+<span className="dot"></span>
+
+NETWORK SECURE
+
+</div>
+
+<div>
+
+<span className="dot"></span>
+
+AI READY
+
+</div>
+
+</div>
+
+)
+
+}
