ef3a2dbe926543eb632c134b44ce824d70441a20
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-14 17:12:58 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import Hero from "../components/hero/Hero";
 import About from "../components/about/About";
+import Skills from "../components/skills/Skills";
 
 export default function Home() {
+
   return (
+
     <>
+
       <Hero />
+
       <About />
+
+      <Skills />
+
     </>
+
   );
+
 }
