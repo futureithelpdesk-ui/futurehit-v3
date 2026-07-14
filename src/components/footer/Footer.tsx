a77a9cb0e5fb26975a958376ef2da9026b3d2b12
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-14 19:21:50 +0530

    Create Footer.tsx

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,33 @@
+import "./Footer.css";
+
+export default function Footer(){
+
+return(
+
+<footer className="footer">
+
+<h2>
+
+FUTURE
+
+</h2>
+
+<p>
+
+Cyber Intelligence • Privacy • Security
+
+</p>
+
+<div className="footer-line"></div>
+
+<span>
+
+© 2026 Future. All Rights Reserved.
+
+</span>
+
+</footer>
+
+)
+
+}
